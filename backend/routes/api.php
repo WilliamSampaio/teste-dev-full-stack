90db675696e104c7498d9acbc2c4494e392896f1
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2025-05-11 16:35:42 -0400

    rotas finalizadas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,9 +4,12 @@ use App\Http\Controllers\Api\V1\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::get('/usuarios', [UsuarioController::class, 'index']);
-    Route::get('/usuarios/{usuario}', [UsuarioController::class, 'show']);
-    Route::post('/usuarios', [UsuarioController::class, 'store']);
-    Route::put('/usuarios/{usuario}', [UsuarioController::class, 'update']);
-    Route::delete('/usuarios/{usuario}', [UsuarioController::class, 'destroy']);
+    // Route::get('/usuarios', [UsuarioController::class, 'index']);
+    // Route::get('/usuarios/{usuario}', [UsuarioController::class, 'show']);
+    // Route::post('/usuarios', [UsuarioController::class, 'store']);
+    // Route::put('/usuarios/{usuario}', [UsuarioController::class, 'update']);
+    // Route::delete('/usuarios/{usuario}', [UsuarioController::class, 'destroy']);
+
+    // Melhoria
+    Route::apiResource('usuarios', UsuarioController::class);
 });
